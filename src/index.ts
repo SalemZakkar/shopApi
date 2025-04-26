a8a8442a4a72ef5fb1631c43b8b673072d487f23
Salem <salem.zakkar@unifi.solutions>
2025-04-27 01:07:07 +0300

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import express from "express";
 
 let app = express();
-
 import * as cors from "cors";
-
 import {config} from "dotenv";
 import qs from "qs";
 import {errorHandlerMiddleWare} from "./common/middleware/errorMiddleware";
-import {CategoryModel} from "./categories/models/category-model";
-import {sendSuccess} from "./common/utils/utils/sendResponse";
 
 config()
 
@@ -17,17 +13,6 @@ app.set('query parser', (str: string) => qs.parse(str));
 app.use(cors.default())
 
 app.use(express.json())
-
-app.use((req, res, next) => {
-     CategoryModel.find().then((e) => {
-        sendSuccess(
-            res,
-            {
-                data: e
-            }
-        )
-    })
-})
 
 app.use(errorHandlerMiddleWare)
 
